7e1a863dc7d937464758d6ef7554bd8f17c7ba67
Eric Xu <xeric.2002@gmail.com>
2023-10-21 22:31:26 -0400

    Add hero to detail pages

--- html_gen/static/js/scripts.js
+++ html_gen/static/js/scripts.js
@@ -15,3 +15,18 @@ dropup.addEventListener("pointerout", (e) => {
   if (e.pointerType !== "mouse") return;
   dropup.classList.remove(classNameDropupOpen);
 });
+
+const heroBtn = document.querySelector("#header .hero__btn");
+const heroTxt = document.querySelector("#header .hero__txt");
+const classNameIsVisible = "hero__txt--hidden";
+heroBtn.addEventListener("click", () => {
+  heroTxt.classList.toggle(classNameIsVisible);
+});
+heroBtn.addEventListener("pointerover", (e) => {
+  if (e.pointerType !== "mouse") return;
+  heroTxt.classList.add(classNameIsVisible);
+});
+heroBtn.addEventListener("pointerout", (e) => {
+  if (e.pointerType !== "mouse") return;
+  heroTxt.classList.remove(classNameIsVisible);
+});
